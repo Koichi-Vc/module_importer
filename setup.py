5fae03d6686d17f48ff7e619688b679f98279e12
Fukuro Loce <wslerne@gmail.com>
2025-02-07 00:43:09 +0900

    バージョン0.xを1.0.0に更新

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'module_importer',
-    version='0.x',
+    version='1.0.0',
     packages = find_packages(),
     package_dir = {'':'src'}
 )
